079911b512ebb57622f8531e5f72a430d9b79918
Kyle Hoernlein <khoernlein90@gmail.com>
2018-04-26 15:05:49 -0400

    added mongo

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
+require("./routes/authRoutes")(app);
+
 const PORT = process.env.PORT || 3000;
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken", accessToken);
-      console.log("refresh token", refreshToken);
-      console.log("profile", profile);
-    }
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
-
 app.listen(PORT, function() {
-  console.log("we running");
+  console.log("we running on", PORT);
 });
