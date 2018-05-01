cc7190eee8b806bf2f6b0f94351f775f87b2c561
Kyle Hoernlein <khoernlein90@gmail.com>
2018-05-01 14:50:01 -0400

    added server proxy

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, function() {
   console.log("we running on", PORT);
 });
